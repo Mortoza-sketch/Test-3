738594d0bcaa397ac22d49a1ecf402827a9b5e08
Mortoza Tamjid <ad@gmail.com>
2020-02-18 15:13:44 -0500

    my first commit

---
+++ shortjava.java
@@ -0,0 +1,14 @@
+
+public class shortjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		short a=10000;	// four zero is the maximum :D, if you add more than that there will be an error	
+	      
+	      System.out.println(a);
+			
+			
+		}
+
+	}
